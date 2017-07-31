6c19d2f726f4146396e73a519db86a778c4cce50
khani <mojtaba.khani@yahoo.com>
2017-07-31 20:10:09 +0430

    create route for webhook.

--- src/Route.php
+++ src/Route.php
@@ -5,7 +5,30 @@ use XB\theory\Shoot;
 use XB\telegramObjects\Update;
 // use XB\telegramMethods\getUpdates;
 
+Route::get('telegram/webhook',function(){
 
+    $data=json_decode(file_get_contents('php://input'),true);
+
+    if(json_last_error()){
+        $trace = debug_backtrace();
+            trigger_error(
+                "{$this->name}: telegram's bot response is invalid format :  ".json_last_error_msg() .
+                ' in ' . $trace[0]['file'] .
+                ' on line ' . $trace[0]['line'],
+                E_USER_NOTICE);
+        return false;
+    }
+
+
+    Shoot::$update=new XB\telegramObjects\Update($data,'object');
+
+    Shoot::load();
+
+    Shoot::fire();
+
+    return Shoot::$result;
+    return 'res:   '.file_get_contents('php://input');
+});
 
 Route::get('telegram/getUpdate',function(){
     $o=new XB\telegramMethods\getUpdates(['offset'=>-1]);
